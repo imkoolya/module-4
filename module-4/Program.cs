82b6e57d3c4ef7ee984e015f0bca211504a2b67e
imkoolya <imkoolya@gmail.com>
2023-12-16 12:45:14 +0300

    updated program.cs, task 4.1.17

--- module-4/Program.cs
+++ module-4/Program.cs
@@ -8,12 +8,32 @@ class MainClass
 {
     public static void Main(string[] args)
     {
-        int a;
-        int b;
-        double x;
-        double y;
+        Console.WriteLine("Напишите свой любимый цвет на английском с маленькой буквы");
 
-        bool result = a < b | x > y;
+        var color = Console.ReadLine();
+
+        if (color == "red")
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is red!");
+        }
+
+        else if (color == "green")
+        {
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is green!");
+        }
+        else
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.ForegroundColor = ConsoleColor.Black;
+
+            Console.WriteLine("Your color is cyan!");
+        }
     }
 }
 
