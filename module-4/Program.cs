35187d512cc5221a7d663b90b438076c1f3fc679
imkoolya <imkoolya@gmail.com>
2023-12-16 12:01:08 +0300

    Добавьте файлы проекта.

---
+++ module-4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+
+    }
+}
+
+
+
